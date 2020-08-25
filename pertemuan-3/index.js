9ca745b0d3e6b5c7310586c5860ce5822efdb967
Khrisna224 <khrisnasemara@gmail.com>
2020-08-25 14:45:30 +0800

    pertemuan 3

---
+++ pertemuan-3/index.js
@@ -0,0 +1,33 @@
+//Materi javascript
+//variabel
+//operator
+//Array
+//Object
+//Fungsi
+//Javascript Conditional
+//Javascript loop
+
+console.log("Indonesia");
+
+let name = "Mikoto-";
+let umur = 21;
+umur = 23;
+const age = 30;
+let rank = 10978;
+let playing = true;
+
+console.log("Hi "+ name);
+console.log(rank);
+
+console.log(typeof(name));
+console.log(typeof(name, rank, playing));
+//typeof untuk mengetahui tipe data
+
+let angka= 5
+angka **= 2;
+console.log(angka);
+
+//Ternary operator
+let makanan = "daging";
+let jenisHewan = makanan === "daging"  ? "karnivora" : "herbivora";
+console.log(jenisHewan);
