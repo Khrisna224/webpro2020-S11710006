207c6eab17559138d75c9d88c58eaf829f0bdd34
Khrisna Semara <32382686+Khrisna224@users.noreply.github.com>
2020-11-10 20:23:57 +0800

    pertemuan 20

--- pertemuan-20/learn-react/src/App.js
+++ pertemuan-20/learn-react/src/App.js
@@ -1,19 +1,25 @@
 import logo from './logo.svg';
 import './App.css';
-import About from './about'
 
 function App() {
   return (
     <div className="App">
-      <h1>Hi</h1>
-      <Hello />
-      <About />
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
     </div>
   );
-}
-
-const Hello = () =>{
-  return <h1>Hello</h1>
 }
 
 export default App;
